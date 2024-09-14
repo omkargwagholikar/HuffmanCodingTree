aba2cb191affc3b7e275b182b5eb354796a68702
omkargwagholikar <omkargwagholikar@gmail.com>
2024-09-14 20:51:17 +0530

    Fixed bug

--- Nodes/HuffLeafNode.java
+++ Nodes/HuffLeafNode.java
@@ -8,12 +8,15 @@ public class HuffLeafNode extends HuffBaseNode{
         weight = Long.MIN_VALUE;
     }
     public HuffLeafNode(char val, long weight) {
+        isLeaf = true;
         this.val = val;
         this.weight = weight;
     }
     public void get() {
         System.out.println("everything works");
     }
+
+    public boolean isLeaf() {return isLeaf;}
     
     public char getChar() {return val;}
 }
